e0c0f92c770a13740aed0a9679df65ecd0fc654c
Robert Bendun <robert@bendun.cc>
2025-05-06 16:38:13 +0200

    Infinite loop with(out) tail recursion

--- b.c
+++ b.c
@@ -1,11 +1,11 @@
 #include <assert.h>
 #include <ctype.h>
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 struct string_builder
 {
---
+++ examples/yes.b
@@ -0,0 +1,7 @@
+/* TODO: Support either loops or tail recursion */
+main() {
+	extrn putchar;
+	putchar('y');
+	putchar('*n');
+	main();
+}
